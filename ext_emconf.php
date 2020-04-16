e14eefc2b4d1127db4e0056c5d1f99dfa0fb2f23
Christian Hackl <hackl.chris@googlemail.com>
2020-04-16 15:43:17 +0200

    added TYPO3 10 to require/depends

--- ext_emconf.php
+++ ext_emconf.php
@@ -18,11 +18,11 @@ $EM_CONF['hh_slider'] = [
     'author_email' => 'chackl@hauer-heinrich.de',
     'author_company' => 'www.hauer-heinrich.de',
     'state' => 'stable',
-    'version' => '0.1.61',
+    'version' => '0.1.62',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-9.5.99',
-            'fluid_styled_content' => '9.5.0-9.9.99'
+            'typo3' => '9.5.0-10.4.99',
+            'fluid_styled_content' => '9.5.0-10.4.99'
         ],
         'conflicts' => [
         ],
