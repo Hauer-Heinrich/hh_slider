f8874a965bfd25baa35f3421c88acd5a5ece041e
Christian Hackl <hackl.chris@googlemail.com>
2020-11-30 17:44:54 +0100

    [TASK] code format

--- Classes/Hooks/PageLayoutViewHook.php
+++ Classes/Hooks/PageLayoutViewHook.php
@@ -6,12 +6,12 @@ use TYPO3\CMS\Backend\View\PageLayoutView;
 
 class PageLayoutViewHook {
     public function contentIsUsed(array $params, PageLayoutView $parentObject): bool {
-        if($params["used"]) {
+        if($params['used']) {
             return true;
         }
 
         $record = $params['record'];
-        if($params["used"] === false && $record["colPos"] === 999) {
+        if($params['used'] === false && $record['colPos'] === 999) {
             return $record['colPos'] === 999 && !empty($record['tx_hhslider_child_content_parent']);
         }
 
