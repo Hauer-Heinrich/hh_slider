eb85b18944fb16cea2bbaf50c9b448970790558a
Christian Hackl <hackl.chris@googlemail.com>
2026-04-22 14:36:21 +0200

    [BREAKING][TASK] delete optional option "automatically add TypoScript" (2)

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -4,16 +4,9 @@ defined('TYPO3') || die('Access denied.');
 use \TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 call_user_func(function(string $extensionKey) {
-    // Typo3 extension manager gearwheel icon (ext_conf_template.txt)
-    $extensionConfiguration = isset($GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS'][$extensionKey]) ? $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS'][$extensionKey] : [];
-    $extensionConfig = isset($extensionConfiguration['config']) ? $extensionConfiguration['config'] : [];
-
-    // If automatically include of TypoScript is disabled, then you can include it in the (BE) static-template select-box
-    if ($extensionConfig['typoScript'] === '0') {
-        ExtensionManagementUtility::addStaticFile(
-            $extensionKey,
-            'Configuration/TypoScript',
-            'Hauer-Heinrich - Slider'
-        );
-    }
+    ExtensionManagementUtility::addStaticFile(
+        $extensionKey,
+        'Configuration/TypoScript',
+        'Hauer-Heinrich - Slider'
+    );
 }, 'hh_slider');
